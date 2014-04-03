d75a9dabb5c9a8cb5a57f284b714117e2a4c94f9
sonecabr <devel.andrerocha@gmail.com>
2014-04-02 23:29:27 -0300

    primeira aula

---
+++ aula1/Teste.java
@@ -0,0 +1,9 @@
+import java.util.*;  
+public class Teste {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe um numero");
+		Double numero = Double.parseDouble(scan.nextLine());
+		System.out.println("ass" + numero);
+	}
+}
