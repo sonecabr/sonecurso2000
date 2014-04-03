f12899643d653d7da44b0915c3fcaba1a224d149
Andre Rocha <devel.andrerocha@gmail.com>
2014-04-03 00:58:23 -0300

    Delete Teste.java

--- aula1/Teste.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;  
-public class Teste {
-	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Informe um numero");
-		Double numero = Double.parseDouble(scan.nextLine());
-		System.out.println("ass" + numero);
-	}
-}
